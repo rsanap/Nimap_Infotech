07979f36863c539e0066374f97ffa26df2a37e28
rsanap <115036751+rsanap@users.noreply.github.com>
2025-01-04 03:08:44 +0530

    Update Product.cs
    Add Rowversion property for automatic update and concurrency

--- Models/Product.cs
+++ Models/Product.cs
@@ -1,5 +1,6 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,8 +10,10 @@ namespace NimapInfotech.Models
     {
         public int ProductId { get; set; }
         public string ProductName { get; set; }
-        public int CategoryId { get; set; }
-
+        public int? CategoryId { get; set; }        
+        [Timestamp]
+        public byte[] RowVersion { get; set; }
         public virtual Category Category { get; set; }
     }
+
 }
