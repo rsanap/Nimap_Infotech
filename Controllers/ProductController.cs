71e3050c3c11f3e8edbdb96333460aaed203c348
rsanap <115036751+rsanap@users.noreply.github.com>
2025-01-04 02:59:21 +0530

    Update ProductController.cs
    Updated the action edit and delete for product controller

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -1,10 +1,11 @@
-﻿using NimapInfotech.Models;
+using NimapInfotech.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
 
 namespace NimapInfotech.Controllers
 {
@@ -15,7 +16,6 @@ namespace NimapInfotech.Controllers
         // GET: Product/Create
         public ActionResult Create()
         {
-           
             ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName");
             return View();
         }
@@ -27,7 +27,6 @@ namespace NimapInfotech.Controllers
         {
             if (ModelState.IsValid)
             {
-               
                 db.Products.Add(product);
                 db.SaveChanges();
                 return RedirectToAction("Index");
@@ -37,12 +36,87 @@ namespace NimapInfotech.Controllers
             return View(product);
         }
 
+        // GET: Product/Edit
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        // POST: Product/Edit
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductId, ProductName, CategoryId")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+               
+                Product existingProduct = db.Products.Find(product.ProductId);
+                if (existingProduct == null)
+                {
+                    return HttpNotFound();
+                }
+
+              
+                existingProduct.ProductName = product.ProductName;
+                existingProduct.CategoryId = product.CategoryId;
+
+                db.Entry(existingProduct).State = EntityState.Modified;
+                db.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.CategoryId = new SelectList(db.Categories, "CategoryId", "CategoryName", product.CategoryId);
+            return View(product);
+        }
+
+        // GET: Product/Delete
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(System.Net.HttpStatusCode.BadRequest);
+            }
+
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+
+            return View(product);
+        }
+
+        // POST: Product/Delete
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
         public ActionResult Index(int page = 1)
         {
             const int pageSize = 10;
 
             var totalProducts = db.Products.Count();
 
+           
             var products = db.Products.Include(p => p.Category)
                                       .OrderBy(p => p.ProductId)
                                       .Skip((page - 1) * pageSize)
@@ -51,7 +125,7 @@ namespace NimapInfotech.Controllers
 
             ViewBag.Page = page;
             ViewBag.TotalPages = (int)Math.Ceiling((double)totalProducts / pageSize);
-            ViewBag.TotalProducts = totalProducts;  
+            ViewBag.TotalProducts = totalProducts;
 
             return View(products);
         }
